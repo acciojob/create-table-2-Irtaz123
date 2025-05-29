a2dca63729c5166a9d1c9ad7cae6042d0c651c1e
Irtaz123 <121966458+Irtaz123@users.noreply.github.com>
2025-05-29 14:43:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 function createTable() {
     //Write your code here
-  
+let rows = prompt("enter the rows")
+let column = prompt("enter the column")	
+let mytable = document.querySelector("#myTable")	
+
+for (let index = 0; index < rows; index++) {
+	let tr = document.createElement("tr")
+	for (let j = 0; j < column; j++) {
+	let td = document.createElement("td")
+		td.innerText = "a"
+		tr.append(td)
+	}
+	
+
+mytable.append(tr)
+}
 }
